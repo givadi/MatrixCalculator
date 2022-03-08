7b99d791445ddcd112a46edb80242ffbe453ef13
Yuliya <givadi@mail.ru>
2022-03-08 13:56:38 +0300

    Added options and conditions, created two Matrices

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,76 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        boolean stat = true;
+        int option;
+
+        while(stat){
+            System.out.println("To start enter 1. To exit enter 2");
+            option = in.nextInt();
+            switch(option){
+                case 1:
+                    stat = createProblem(option);
+                    break;
+                case 2:
+                default:
+                    stat = false;
+                    break;
+            }
+        }
+        System.out.println("Calculator now terminated");
+    }
+
+    public static boolean createProblem(int i){
+        Scanner in = new Scanner(System.in);
+        boolean condition;
+
+        int contStat;
+        boolean statusC;
+
+        int frows;
+        int fcolumns;
+        int srows;
+        int scolumns;
+
+        Matrix matrix1;
+        Matrix matrix2;
+
+        System.out.println("Enter number of rows in first matrix");
+        frows = in.nextInt();
+        String operator;
+        System.out.println("Enter number of columns in first matrix");
+        fcolumns = in.nextInt();
+        System.out.println("Enter number of rows in second matrix");
+        srows = in.nextInt();
+        System.out.println("Enter number of columns in second matrix");
+        scolumns = in.nextInt();
+
+        matrix1 = new Matrix(frows, fcolumns);
+        matrix1.setLabel("First");
+
+        matrix2 = new Matrix(srows, scolumns);
+        matrix2.setLabel("Second");
+
+        matrix1.setMatrixValues();
+        matrix2.setMatrixValues();
+
+        System.out.println("Type 1 if you would like to continue using the calcultor,\n type 2 if you would like to exit");
+        contStat = in.nextInt();
+        switch(contStat){
+            case 1:
+                condition = true;
+                break;
+            case 2:
+            default:
+                condition = false;
+                break;
+        }
+        return condition;
     }
 }
