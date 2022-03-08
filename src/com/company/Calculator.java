bcb123227f41cd2e0331258f069ff2b8899159e0
Yuliya <givadi@mail.ru>
2022-03-08 14:03:21 +0300

    Created Calculator test class, added test for checking if valid to add, added methods to Calculator

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class Calculator {
+    private Matrix matrix1;
+    private Matrix matrix2;
+
+    Calculator(Matrix matrix1, Matrix matrix2)
+    {
+        this.matrix1 = matrix1;
+        this.matrix2 = matrix2;
+    }
+
+    public boolean checkIfValidToAdd()
+    {
+        int numRows1 = matrix1.getNumRows();
+        int numColumns1 = matrix1.getNumColumns();
+        int numRows2 = matrix2.getNumRows();
+        int numColumns2 = matrix2.getNumColumns();
+        return numRows1 == numRows2 && numColumns1 == numColumns2;
+    }
 }
---
+++ src/com/company/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void testCheckIfValidToAdd() {
+        Matrix matrix1 = new Matrix(2,2);
+        Matrix matrix2 = new Matrix(2,2);
+
+        Calculator calculator = new Calculator(matrix1, matrix2);
+        assertEquals(true, calculator.checkIfValidToAdd());
+    }
+}
